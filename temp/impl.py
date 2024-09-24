4b7d2a9c83d28298f2cccbfa66b8499630b83dd8
YitongGu <yitongg3@gmail.com>
2024-09-24 15:37:07 +0000

    cherry pick

--- temp/impl.py
+++ temp/impl.py
@@ -13,9 +13,14 @@ assistant
 import numpy as np
 
 def transform_grid(input_grid: np.ndarray) -> np.ndarray:
+<<<<<<< HEAD
     output_grid = input_grid.copy()
     for i in range(1, input_grid.shape[0] - 1):
         for j in range(1, input_grid.shape[1] - 1):
             if input_grid[i, j]!= 0:
                 output_grid[i, j] = input_grid[i-1, j]
     return output_grid
+=======
+    input_grid = np.sort(input_grid)[::-1]
+    return np.array([input_grid[:5], input_grid[5:10], input_grid[10:15], input_grid[15:20]])
+>>>>>>> 968a5d0 (flip done)
