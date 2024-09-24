c53f90c6fdbbb9a9a19cd3658fb4d38c8afda4f1
YitongGu <yitongg3@gmail.com>
2024-09-24 01:40:00 +0000

    temp

--- temp/impl.py
+++ temp/impl.py
@@ -8,17 +8,14 @@ from collections import Counter
 import math
 np.int = int
 
-
+assistant
 
 import numpy as np
 
 def transform_grid(input_grid: np.ndarray) -> np.ndarray:
-    color = input_grid[0][0]
-    output_grid = np.zeros_like(input_grid)
-    for i in range(input_grid.shape[0]):
-        for j in range(input_grid.shape[1]):
-            if i == 0:
-                output_grid[i][j] = color
-            else:
-                output_grid[i][j] = input_grid[i-1][j]
+    output_grid = input_grid.copy()
+    for i in range(1, input_grid.shape[0] - 1):
+        for j in range(1, input_grid.shape[1] - 1):
+            if input_grid[i, j]!= 0:
+                output_grid[i, j] = input_grid[i-1, j]
     return output_grid
