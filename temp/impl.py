9e18d1522cf6c5937a94d8e949199d50a8cd493b
YitongGu <yitongg3@gmail.com>
2024-09-25 03:32:53 +0000

    flip&fill po done

--- temp/impl.py
+++ temp/impl.py
@@ -14,13 +14,17 @@ import numpy as np
 
 def transform_grid(input_grid: np.ndarray) -> np.ndarray:
     rows, cols = input_grid.shape
-    output_grid = np.zeros((rows, cols))
+    output_grid = np.zeros_like(input_grid)
     
     for i in range(rows):
         for j in range(cols):
-            if input_grid[i, j]!= 0:
-                output_grid[i, j] = input_grid[i, j]
+            if input_grid[i, j] == 0:
+                continue
+            if input_grid[i, j] == 8:
+                output_grid[i, cols - 1] = input_grid[i, j]
+            elif input_grid[i, j] == 4:
+                output_grid[rows - 1, j] = input_grid[i, j]
             else:
-                output_grid[i, j] = 0
+                output_grid[i, j] = input_grid[i, j]
     
     return output_grid
